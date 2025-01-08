3d05c20f43a681b3b81d0b78cd1163382860f5c5
Angelo Leite <angelolgoes13@gmail.com>
2025-01-08 10:09:05 -0300

    feat: continue building request form

--- src/app/pages/request-form/request-form.component.ts
+++ src/app/pages/request-form/request-form.component.ts
@@ -1,34 +1,91 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButton } from '@angular/material/button';
+import {
+	FormControl,
+	FormGroupDirective,
+	FormsModule,
+	NgForm,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { COPY_DATA, CopyInterface } from '../../models/copy.interface';
+import { RequestInterface } from '../../models/request.interface';
+import { IconPipe } from '../../pipes/icon.pipe';
+import { MatSelectModule } from '@angular/material/select';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
+	isErrorState(
+		control: FormControl | null,
+		form: FormGroupDirective | NgForm | null
+	): boolean {
+		const isSubmitted = form && form.submitted;
+		return !!(
+			control &&
+			control.invalid &&
+			(control.dirty || control.touched || isSubmitted)
+		);
+	}
 }
 
 @Component({
-  selector: 'app-request-form',
-  imports: [MatButton, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
-  templateUrl: './request-form.component.html',
-  styleUrl: './request-form.component.scss'
+	selector: 'app-request-form',
+	imports: [
+		MatButton,
+		FormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		ReactiveFormsModule,
+		MatTableModule,
+		MatIconModule,
+		MatTooltipModule,
+		IconPipe,
+		MatButtonModule,
+		MatSelectModule,
+	],
+	templateUrl: './request-form.component.html',
+	styleUrl: './request-form.component.scss',
 })
 export class RequestFormComponent {
-  selectedFile: any = null;
+	selectedFile: any = null;
+	dataSource = new MatTableDataSource<CopyInterface>(COPY_DATA);
 
-  copyNumFormControl = new FormControl('', [Validators.required, Validators.min(1)]);
+	times: number[] = [48, 24, 12, 4, 2, 1];
 
-  matcher = new MyErrorStateMatcher();
+	displayedColumns: string[] = [
+		'file_name',
+		'page_count',
+		'copy_count',
+		'actions',
+	];
 
-  onFileSelected(event: any): void {
-      this.selectedFile = event.target.files[0] ?? null;
-      console.log(this.selectedFile);
-      
-  }
+	allowedActions: string[] = ['Excluir'];
+
+	copyNumFormControl = new FormControl('', [
+		Validators.required,
+		Validators.min(1),
+	]);
+
+	matcher = new MyErrorStateMatcher();
+
+	onFileSelected(event: any): void {
+		this.selectedFile = event.target.files[0] ?? null;
+		console.log(this.selectedFile);
+	}
+
+	checkDisabled(action: string, element: RequestInterface) {
+		if (element.conclusion_date) {
+			if (action == 'Concluir' || action == 'Editar') {
+				return true;
+			}
+		}
+		return false;
+	}
 }
